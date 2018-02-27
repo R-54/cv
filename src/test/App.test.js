2e8ca7a141276f71349048bed69bacd16853d2f3
ivan rodriguez soria <vendeta.ivan@gmail.com>
2018-02-27 15:18:40 -0600

    All test passed

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -20,19 +20,34 @@ describe(<App />, () => {
     ReactDOM.unmountComponentAtNode(div);
   });
 
-  it('renders a <Main /> component', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.contains(<Main />)).toBe(true);
-  });
-
-  it('renders an <PersonalInformation /> component', () => {
+  it('renders a <PersonalInformation /> component', () => {
     const wrapper = shallow(<Main />);
-    expect(wrapper.contains(<PersonalInformation />)).toBe(true);
+    expect(wrapper.find(PersonalInformation)).toHaveLength(1);
   });
 
   it('renders a <Description /> component', () => {
     const wrapper = shallow(<Main />);
-    expect(wrapper.contains(<Description />)).toBe(true);
+    expect(wrapper.find(Description)).toHaveLength(1);
+  });
+
+  it('renders 3 <Sections /> in <PersonalInformation /> component', () => {
+    const sections = [
+      {title: 'title 1', content: [{ data: 'data 1' }]},
+      {title: 'title 2', content: [{ data: 'data 1' }]},
+      {title: 'title 3', content: [{ data: 'data 1' }]}
+    ];
+    const wrapper = mount(<PersonalInformation sections={sections} />);
+    expect(wrapper.find('SectionContent')).toHaveLength(3);
+  });
+
+  it('renders 3 <Sections /> in <Description /> component', () => {
+    const sections = [
+      {title: 'title 1', content: [{ data: 'data 1' }]},
+      {title: 'title 2', content: [{ data: 'data 1' }]},
+      {title: 'title 3', content: [{ data: 'data 1' }]}
+    ];
+    const wrapper = mount(<Description sections={sections} />);
+    expect(wrapper.find('SectionContent')).toHaveLength(3);
   });
 
 });
